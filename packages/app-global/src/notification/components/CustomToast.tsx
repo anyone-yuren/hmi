759e6aaa7b5b458efe66ede5b51f8cb0594bfef1
niyonggui <niyg@mw-robot.com>
2025-07-25 15:17:30 +0800

    feat(deployer): 新增异常与避障逻辑对接

--- packages/app-global/src/notification/components/CustomToast.tsx
+++ packages/app-global/src/notification/components/CustomToast.tsx
@@ -1,24 +1,31 @@
 'use client';
 
+import CloseIcon from '@mui/icons-material/Close';
 import { Button } from 'antd';
+import { ReactNode } from 'react';
 import { toast as sonnerToast } from 'sonner';
 /** I recommend abstracting the toast function
  *  so that you can call it without having to use toast.custom everytime. */
-export function toast(toast: Omit<ToastProps, 'id'>) {
+export function toast(
+  toast: Omit<ToastProps, 'id'>,
+  position?: 'top-center' | 'top-right' | 'top-left' | 'bottom-center' | 'bottom-right' | 'bottom-left',
+) {
   return sonnerToast.custom(
     (id) => (
       <Toast
         id={id}
         title={toast.title}
         description={toast.description}
+        className={toast.className}
         button={{
-          label: toast.button.label,
+          label: toast.button?.label,
           onClick: () => console.log('Button clicked'),
         }}
       />
     ),
     {
       duration: Infinity,
+      position: position ?? 'top-center',
     },
   );
 }
@@ -26,35 +33,49 @@ export function toast(toast: Omit<ToastProps, 'id'>) {
 /** A fully custom toast that still maintains the animations and interactions. */
 function Toast(props: ToastProps) {
   const { title, description, button, id } = props;
-
   return (
-    <div className='flex rounded-lg bg-[#facc15] shadow-lg ring-1 ring-black/5 w-full min-w-[400px] items-center p-4'>
+    <div
+      className={`flex rounded-lg bg-[#facc15] shadow-lg ring-1 ring-black/5 w-full min-w-[500px] mx-auto items-center p-4 ${props.className}`}
+    >
       <div className='flex flex-1 items-center'>
         <div className='w-full'>
           <p className='text-lg font-medium text-gray-900'>{title}</p>
-          <p className='mt-1 text-lg text-white font-bold'>{description}</p>
+          <div className='mt-1 text-white font-bold'>{[description]}</div>
         </div>
       </div>
-      <div className='ml-5 shrink-0 rounded-md text-sm font-medium '>
-        <Button
-          type='link'
-          onClick={() => {
-            button.onClick();
-            sonnerToast.dismiss(id);
-          }}
-        >
-          {button.label}
-        </Button>
-      </div>
+
+      {/* <CloseOutlined
+        onClick={() => sonnerToast.dismiss(id)}
+        className='absolute top-2 right-2 '
+        style={{
+          fontSize: '36px !important',
+        }}
+      /> */}
+      <CloseIcon onClick={() => sonnerToast.dismiss(id)} fontSize='large' className='absolute top-2 right-2 ' />
+
+      {button?.label ? (
+        <div className='ml-5 shrink-0 rounded-md text-sm font-medium '>
+          <Button
+            type='link'
+            onClick={() => {
+              button?.onClick();
+              sonnerToast.dismiss(id);
+            }}
+          >
+            {button?.label}
+          </Button>
+        </div>
+      ) : null}
     </div>
   );
 }
 
 interface ToastProps {
   id: string | number;
-  title: string;
-  description: string;
-  button: {
+  title: string | ReactNode;
+  description: string | ReactNode;
+  className?: string;
+  button?: {
     label: string;
     onClick: () => void;
   };
--- packages/app-global/src/notification/hooks/useNotification.tsx
+++ packages/app-global/src/notification/hooks/useNotification.tsx
@@ -1,8 +1,12 @@
 import { useWebSocket } from 'ahooks';
-import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import { isEqual } from 'lodash-es';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { toast as sonnerToast } from 'sonner';
 import { toast } from '../components/CustomToast';
 import useObsError from './useObsError';
+
 // 动态获取当前 host
 const currentHost = window.location.hostname;
 // 使用相对路径，Vite 会自动处理代理
@@ -10,9 +14,10 @@ const HYBRID_URL = import.meta.env.DEV
   ? '/ws10009' // 开发环境使用代理
   : `ws://${currentHost}:10009`; // 生产环境使用真实地址
 
-const hashMap: any = {};
 export const useNotification = () => {
   const { t } = useTranslation();
+  const errorMsg = useRef<any>();
+  const obsMsg = useRef<any>();
   const { getObsMsg } = useObsError();
   const [obsInfo, setObsInfo] = useState<any>();
   const [errorMessage, setErrorMessage] = useState<any>();
@@ -31,16 +36,60 @@ export const useNotification = () => {
       }
       if (message?.data?.includes('/sirius/topics/error_description')) {
         // TODO 此处需要改造成json格式
-        // const data = JSON.parse(message.data);
-        // setErrorMessage((prev) => {
-        //   if (prev?.type === data.type) {
-        //     return prev;
-        //   }
-        //   return data;
-        // });
+        const data = JSON.parse(message.data);
+        setErrorMessage((prev) => {
+          if (isEqual(prev, data.data)) {
+            return prev;
+          }
+          return data.data;
+        });
       }
     },
   });
+  useEffect(() => {
+    if (errorMessage && errorMessage?.length) {
+      if (errorMsg.current) {
+        sonnerToast.dismiss(errorMessage.current);
+      }
+      errorMsg.current = toast({
+        title: (
+          <>
+            <div className='flex items-center justify-between'>
+              <div className=' text-white font-bold'>{t('common.errorMsg.title')}</div>
+            </div>
+          </>
+        ),
+        description: (
+          <div className=''>
+            {errorMessage.map((item: string, index) => {
+              return (
+                <div key={item.error_code} className='mb-2'>
+                  <p>
+                    {index + 1} {item?.description}
+                  </p>
+                  <div className=''>
+                    <p className='opacity-50'>{item?.solution}</p>
+                    <p className='opacity-50'>{dayjs.unix(item?.time).format('YYYY-MM-DD HH:mm:ss')}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ),
+        className: ' bg-[#d90707]',
+        // button: {
+        //   label: t('common.obsError.view'),
+        //   onClick: () => {
+        //     console.log('查看通知');
+        //   },
+        // },
+      });
+    } else {
+      // errorMsg.current?.close();
+      sonnerToast.dismiss(errorMsg.current);
+    }
+  }, [errorMessage]);
+
   useEffect(() => {
     if (obsInfo !== 1 && getObsMsg(obsInfo)) {
       toast({
