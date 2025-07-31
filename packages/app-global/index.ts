189026f82fc12e459a87eeb1ea767393a155c82c
wangxiaohua <>
2025-07-31 16:08:23 +0800

    fix(deployer): 恢复任务模块

--- packages/app-global/index.ts
+++ packages/app-global/index.ts
@@ -1,5 +1,5 @@
 import https, { get, post } from './src/https';
 import * as commonServices from './src/https/global';
 import GlobalNotification from './src/notification';
-
-export { commonServices, get, GlobalNotification, https, post };
+import SingleTask from './src/SingleTask';
+export { commonServices, get, GlobalNotification, https, post, SingleTask };
