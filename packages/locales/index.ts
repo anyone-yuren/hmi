9bbc2e0dc04fa190b8ac0f91a7720c407d6ea9a2
wangxiaohua <>
2025-08-06 11:17:53 +0800

    feat(deployer): 补全英语翻译

--- packages/locales/index.ts
+++ packages/locales/index.ts
@@ -4,17 +4,14 @@ import jaJP from './locales/ja-JP';
 import koKR from './locales/ko-KR';
 import zhCN from './locales/zh-CN';
 import origin from './origin.json';
-console.log('[locales]', jaJP, origin);
-
+// origin 是翻译的源文件。每个多语言和这个对比，之前有的就拿出来
+// 先不删除，翻译完后删除
 function replaceChineseValuesWithJapanese(obj: any, chineseToJapaneseMap: Record<string, string>): any {
   if (typeof obj === 'string') {
-    // 如果是字符串，检查是否在映射中有对应的日文翻译
     return chineseToJapaneseMap[obj] || obj;
   } else if (Array.isArray(obj)) {
-    // 如果是数组，递归处理每个元素
     return obj.map((item) => replaceChineseValuesWithJapanese(item, chineseToJapaneseMap));
   } else if (typeof obj === 'object' && obj !== null) {
-    // 如果是对象，递归处理每个属性值
     const result: any = {};
     for (const key in obj) {
       if (obj.hasOwnProperty(key)) {
@@ -23,7 +20,6 @@ function replaceChineseValuesWithJapanese(obj: any, chineseToJapaneseMap: Record
     }
     return result;
   }
-  // 其他类型直接返回
   return obj;
 }
 
@@ -32,7 +28,7 @@ function translateJaJPWithOrigin(jaJP: any, origin: Record<string, string>): any
   return replaceChineseValuesWithJapanese(jaJP, origin);
 }
 
-const translatedJaJP = translateJaJPWithOrigin(jaJP, origin);
+const translatedJaJP = translateJaJPWithOrigin(enUS, origin);
 
 console.log('[locales]', JSON.stringify(translatedJaJP));
 
