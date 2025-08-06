12cbff5dda066dd7d72405c608934117f4b02b06
wangxiaohua <>
2025-08-06 10:44:03 +0800

    feat(deployer): 补全日文key

--- packages/locales/index.ts
+++ packages/locales/index.ts
@@ -3,6 +3,9 @@ import frFR from './locales/fr-FR';
 import jaJP from './locales/ja-JP';
 import koKR from './locales/ko-KR';
 import zhCN from './locales/zh-CN';
+import origin from './origin.json';
+console.log('[locales]', jaJP, origin);
+
 export default {
   'zh-CN': zhCN,
   'en-US': enUS,
