8294843dd1337c0b95800bb61d91a253cb034ee3
wangxiaohua <>
2025-08-06 11:34:09 +0800

    feat(deployer): 法语翻译

--- packages/locales/index.ts
+++ packages/locales/index.ts
@@ -3,9 +3,8 @@ import frFR from './locales/fr-FR';
 import jaJP from './locales/ja-JP';
 import koKR from './locales/ko-KR';
 import zhCN from './locales/zh-CN';
-import origin from './origin.json';
 // origin 是翻译的源文件。每个多语言和这个对比，之前有的就拿出来
-// 先不删除，翻译完后删除
+// 先不删除，翻译完后删除,以下是对比的方法
 function replaceChineseValuesWithJapanese(obj: any, chineseToJapaneseMap: Record<string, string>): any {
   if (typeof obj === 'string') {
     return chineseToJapaneseMap[obj] || obj;
@@ -28,9 +27,9 @@ function translateJaJPWithOrigin(jaJP: any, origin: Record<string, string>): any
   return replaceChineseValuesWithJapanese(jaJP, origin);
 }
 
-const translatedJaJP = translateJaJPWithOrigin(enUS, origin);
+// const translatedJaJP = translateJaJPWithOrigin(frFR, origin);
 
-console.log('[locales]', JSON.stringify(translatedJaJP));
+// console.log('[locales]', JSON.stringify(translatedJaJP));
 
 export default {
   'zh-CN': zhCN,
