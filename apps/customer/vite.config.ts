a1ee009b9ed17a2974850d1e20fcf950bb1e6911
niyonggui <niyg@mw-robot.com>
2025-08-07 09:46:12 +0800

    feat(customer): 首页与施工端同步

--- apps/customer/vite.config.ts
+++ apps/customer/vite.config.ts
@@ -8,6 +8,9 @@ const { VITE_APP_API } = loadEnv(process.env.NODE_ENV as string, root);
 
 export default defineApplicationConfig({
   overrides: {
+    define: {
+      'import.meta.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'false'),
+    },
     optimizeDeps: {
       include: ['@iconify/react', 'lodash-es', 'echarts', 'echarts-for-react'],
     },
@@ -17,25 +20,44 @@ export default defineApplicationConfig({
       port: 4000,
       proxy: {
         '/api': {
-          target: 'http://192.168.2.233:10009',
+          target: `http://${VITE_APP_API}:10009`,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
         '/admin': {
-          target: 'http://192.168.2.233:10001',
+          target: `http://${VITE_APP_API}:10001`,
           changeOrigin: true,
           ws: true,
           rewrite: (path) => path.replace(/^\/admin/, ''),
         },
+        '/tool': {
+          target: `http://${VITE_APP_API}:10020`,
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/tool/, ''),
+        },
+        '/vision': {
+          target: `http://${VITE_APP_API}:10010`,
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/vision/, ''),
+        },
         '/ws10001': {
           // 新增 WebSocket 代理
-          target: 'ws://192.168.2.233:10001',
+          target: `ws://${VITE_APP_API}:10001`,
+          ws: true,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/ws/, ''),
+        },
+        '/ws10010': {
+          // 新增 WebSocket 代理
+          target: `ws://${VITE_APP_API}:10010`,
           ws: true,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/ws/, ''),
         },
         '/ws10009': {
-          target: 'ws://192.168.2.233:10009',
+          target: `ws://${VITE_APP_API}:10009`,
           ws: true,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/ws10009/, ''),
