8704ff05bc52bd31339906b31ab3132ce7ef27cc
niyonggui <niyg@mw-robot.com>
2025-08-14 17:01:50 +0800

    feat(deployer): 新增充电记录

--- packages/app-global/src/Charging/components/chargingHistory/index.tsx
+++ packages/app-global/src/Charging/components/chargingHistory/index.tsx
@@ -1,12 +1,50 @@
-import { Drawer } from 'antd';
+import { useRequest } from 'ahooks';
+import { Button, DatePicker, Drawer, Result, Spin, Table, TimeRangePickerProps } from 'antd';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { SvgIcon } from 'ui';
 import useDrawerClassName from '../../../hooks/useDrawerClassName';
+import { postChargingHistory } from '../../services';
+
+const { RangePicker } = DatePicker;
 interface Props {
   open: boolean;
   onClose: () => {};
 }
 const ChargingHistory = (props: Props) => {
+  const { t } = useTranslation();
   const { open, onClose } = props;
-  const classNames = useDrawerClassName;
+  const classNames = useDrawerClassName();
+  const today = dayjs();
+  const todayStart = dayjs().startOf('day');
+  const [timeRange, setTimeRange] = useState([todayStart, today]);
+  const { data, loading, run } = useRequest(postChargingHistory, {
+    manual: true,
+  });
+  useEffect(() => {
+    if (!open) {
+      return;
+    }
+    run({
+      start_time: todayStart.unix(),
+      end_time: today.unix(),
+    });
+  }, [open]);
+
+  const submit = () => {
+    run({
+      start_time: timeRange[0].unix(),
+      end_time: timeRange[1].unix(),
+    });
+  };
+
+  const rangePresets: TimeRangePickerProps['presets'] = [
+    { label: '最近7天', value: [dayjs().add(-7, 'd'), dayjs()] },
+    { label: '最近14天', value: [dayjs().add(-14, 'd'), dayjs()] },
+    { label: '最近30天', value: [dayjs().add(-30, 'd'), dayjs()] },
+    { label: '最近90天', value: [dayjs().add(-90, 'd'), dayjs()] },
+  ];
   return (
     <Drawer
       closable
@@ -19,10 +57,84 @@ const ChargingHistory = (props: Props) => {
         ...classNames,
         body: '!p-0',
       }}
-      width={'50%'}
+      width={'70%'}
       onClose={onClose}
     >
-      121212
+      <div className='w-full h-full p-4 gap-4 flex flex-col'>
+        <div className='w-full p-2 flex items-center justify-end gap-2 rounded-xl bg-white/20'>
+          <RangePicker
+            showTime={{ format: 'HH:mm' }}
+            format='YYYY-MM-DD HH:mm'
+            onChange={(value, dateString) => {
+              setTimeRange(value);
+            }}
+            onOk={() => {}}
+            defaultValue={[todayStart, today]}
+            presets={[
+              {
+                label: <span aria-label='今天'>今天</span>,
+                value: () => [todayStart, today], // 5.8.0+ support function
+              },
+              ...rangePresets,
+            ]}
+          />
+
+          <Button type='primary' onClick={submit}>
+            查询
+          </Button>
+        </div>
+        {/* 查询结果 */}
+        <div className='w-full h-full bg-white/20 rounded-xl'>
+          {loading ? (
+            <div className='w-full h-full flex items-center justify-center'>
+              <Spin size='large' />
+            </div>
+          ) : data?.data ? (
+            <div className='w-full h-full p-4'>
+              <Table
+                columns={[
+                  {
+                    title: '起始电量',
+                    dataIndex: 'start_power',
+                    key: 'start_power',
+                  },
+                  {
+                    title: '结束电量',
+                    dataIndex: 'end_power',
+                    key: 'end_power',
+                  },
+                  {
+                    title: '充电度数',
+                    dataIndex: 'charging_degree',
+                    key: 'charging_degree',
+                  },
+                  {
+                    title: '开始时间',
+                    dataIndex: 'start_time',
+                    key: 'start_time',
+                    render: (text) => dayjs(text * 1000).format('YYYY-MM-DD HH:mm:ss'),
+                  },
+                  {
+                    title: '结束时间',
+                    dataIndex: 'end_time',
+                    key: 'end_time',
+                    render: (text) => dayjs(text * 1000).format('YYYY-MM-DD HH:mm:ss'),
+                  },
+                ]}
+                dataSource={data?.data}
+                pagination={{
+                  total: data?.data.length,
+                  defaultCurrent: 1,
+                  pageSize: 6,
+                }}
+                rowKey={(record) => record.end_time}
+              />
+            </div>
+          ) : (
+            <Result icon={<SvgIcon name='without' size={340} />} title={t('common.noData')} />
+          )}
+        </div>
+      </div>
     </Drawer>
   );
 };
--- packages/app-global/src/Charging/services/index.ts
+++ packages/app-global/src/Charging/services/index.ts
@@ -1,29 +1,5 @@
 import { get, post } from '@gbeata/app-global';
 
-export const postFloorList = () => post('/navigation/floor_list');
-
-export const addFloor = (num: any) => {
-  return post('/navigation/add_new_floor_number', {
-    floor_number: num,
-  });
-};
-
-export const getBasicControlParam = (): Promise<any> => {
-  return get('/robot_config/control_param/basic_control_param');
-};
-export const postBasicControlParam = (params: any): Promise<any> => {
-  return post('/robot_config/control_param/basic_control_param', params);
-};
-
-// 获取底盘参数
-export const getActionControlParam = (): Promise<any> => {
-  return get('/robot_config/control_param/action_control_param');
-};
-// 更新底盘参数
-export const postActionControlParam = (params: any): Promise<any> => {
-  return post('/robot_config/control_param/action_control_param', params);
-};
-
 // 获取外设参数
 export const getPeripheralControlParam = (): Promise<any> => {
   return get('/robot_config/peripheral_param/config_interactor_peripheral');
@@ -32,4 +8,9 @@ export const getPeripheralControlParam = (): Promise<any> => {
 // 更新外设参数
 export const postPeripheralControlParam = (params: any): Promise<any> => {
   return post('/robot_config/peripheral_param/config_interactor_peripheral', params);
+};
+
+// 获取充电历史
+export const postChargingHistory = (params: any): Promise<any> => {
+  return post('/mwrobot/get_charge_record', params);
 };
