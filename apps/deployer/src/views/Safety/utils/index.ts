ec0e5d50dd3149cb5d7ee13fad67df32fc4dd701
niyonggui <niyg@mw-robot.com>
2025-09-15 17:34:59 +0800

    feat(deployer): 新增避障数据格式转换

--- apps/deployer/src/views/Safety/utils/index.ts
+++ apps/deployer/src/views/Safety/utils/index.ts
@@ -6,3 +6,18 @@ export const meterToPixel = (meter: number) => {
 export const pixelToMeter = (pixel: number) => {
   return Math.floor(pixel / 100);
 };
+
+// 根据左上角xy与宽高 生成一个【x1,y1,x2,y2】的数组
+export const getRectPoints = (x: number, y: number, width: number, height: number) => {
+  return [0 - y, 0 - x, 0 - (y + height), 0 - (x + width)];
+};
+
+// 根据【x1,y1,x2,y2】数组 生成一个矩形
+export const getRect = (points: number[]) => {
+  return {
+    x: 0 - points[1],
+    y: 0 - points[0],
+    width: 0 - (points[3] - points[1]),
+    height: 0 - (points[2] - points[0]),
+  };
+};
