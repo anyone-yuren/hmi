efd8daf3884a30d420266b2a50321de9a836e491
niyonggui <niyg@mw-robot.com>
2025-09-22 15:52:03 +0800

    feat(deployer): 关闭车辆信息与电池

--- apps/deployer/src/views/Charging/index.tsx
+++ apps/deployer/src/views/Charging/index.tsx
@@ -1,5 +1,7 @@
+import EmptyPage from '@/components/EmptyPage';
 import { SubCharging } from '@gbeata/app-global';
 const Charging = () => {
+  return <EmptyPage title='暂未开放' icon='unavailable' />;
   return (
     <>
       <SubCharging />
