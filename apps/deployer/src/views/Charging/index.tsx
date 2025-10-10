caf8e6a697510a1bbe394bb4741e23fe06ee1dcf
niyonggui <niyg@mw-robot.com>
2025-10-10 14:23:54 +0800

    feat(deployer): 测试截图

--- apps/deployer/src/views/Charging/index.tsx
+++ apps/deployer/src/views/Charging/index.tsx
@@ -1,12 +1,12 @@
 import EmptyPage from '@/components/EmptyPage';
 import { SubCharging } from '@gbeata/app-global';
 const Charging = () => {
-  return <EmptyPage title='暂未开放' icon='unavailable' />;
   return (
     <>
       <SubCharging />
     </>
   );
+  return <EmptyPage title='暂未开放' icon='unavailable' />;
 };
 
 export default Charging;
--- packages/app-global/src/Charging/components/vehicleBattery/index.tsx
+++ packages/app-global/src/Charging/components/vehicleBattery/index.tsx
@@ -182,7 +182,7 @@ const VehicleBattery = () => {
             </div>
           </div>
           {/* 车辆图片与充电效果 */}
-          <div className='absolute bottom-0 left-1/2 -translate-x-1/2 w-3/4'>
+          <div className='absolute bottom-0 left-1/2 -translate-x-1/2 w-2/3'>
             {agvType ? (
               <motion.img
                 src={productImage()}
