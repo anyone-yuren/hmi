bc5a7d3f91b7b57224a09082e75cc984d13322b4
wangxiaohua <>
2025-11-20 17:58:51 +0800

    feat(deployer): 恢复充电的功能

--- apps/deployer/src/components/Header/index.tsx
+++ apps/deployer/src/components/Header/index.tsx
@@ -121,11 +121,7 @@ const GlobalHeader = () => {
       const diff = Number(systemDateTime) - closeChargingTime;
       diff > 10 * 60 * 1000 && setCloseChargingTime(0);
     }
-    // 每次冲完了就给他状态重置了
-    if (powerStatus.charge_status === 1 && closeChargingTime != 0) {
-      setCloseChargingTime(0);
-    }
-  }, [systemDateTime, closeChargingTime, powerStatus]);
+  }, [systemDateTime, closeChargingTime]);
 
   return (
     <div className='flex flex-col h-full items-center justify-between px-4 py-2 text-white '>
@@ -255,11 +251,11 @@ const GlobalHeader = () => {
           onClick={() => {
             setShowChargingDialog(false);
             setCloseChargingTime(new Date().getTime());
-            // setPowerStatus({
-            //   ...powerStatus,
-            //   charge_status: 0,
-            //   // power: 0,
-            // });
+            setPowerStatus({
+              ...powerStatus,
+              charge_status: 0,
+              // power: 0,
+            });
           }}
         />
       )}
