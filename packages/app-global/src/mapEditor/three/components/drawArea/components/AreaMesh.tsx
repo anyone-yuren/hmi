f089c2217d7bbc27e8089ecad3b890ab21e75c1d
niyonggui <niyg@mw-robot.com>
2026-01-06 13:48:32 +0800

    feat(customer): 区域选择与设置大小

--- packages/app-global/src/mapEditor/three/components/drawArea/components/AreaMesh.tsx
+++ packages/app-global/src/mapEditor/three/components/drawArea/components/AreaMesh.tsx
@@ -1,11 +1,14 @@
+import { PivotControls } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
 import { useRef } from 'react';
 import * as THREE from 'three';
+import { MapControls as MapControlsImpl } from 'three-stdlib';
 import { useShallow } from 'zustand/react/shallow';
 import { AreaData, useAreaStore } from '../store/areaStore';
 
 export function AreaMesh({ area }: { area: AreaData }) {
   const { controls } = useThree();
+  const mapControls = controls as MapControlsImpl;
   const meshRef = useRef<THREE.Mesh>(null!);
   const dragging = useRef(false);
 
@@ -24,7 +27,7 @@ export function AreaMesh({ area }: { area: AreaData }) {
   const onDown = (e: any) => {
     e.stopPropagation();
     // 禁用 controls
-    controls && (controls.enablePan = false);
+    controls && (mapControls.enablePan = false);
 
     select([area.id]);
     dragging.current = true;
@@ -52,20 +55,66 @@ export function AreaMesh({ area }: { area: AreaData }) {
       center: finalPos,
     });
     // 启用 controls
-    controls && (controls.enablePan = true);
+    controls && (mapControls.enablePan = true);
   };
 
   return (
-    <mesh
-      ref={meshRef}
-      position={area.center}
-      scale={[area.width, area.height, 1]}
-      onPointerDown={onDown}
-      onPointerMove={onMove}
-      onPointerUp={onUp}
-    >
-      <planeGeometry args={[1, 1]} />
-      <meshBasicMaterial color={selected ? '#fab005' : '#51cf66'} transparent opacity={0.4} />
-    </mesh>
+    <group>
+      <PivotControls
+        visible={selected}
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        fixed
+        scale={80}
+        /** 平移：只允许 XY */
+        activeAxes={[true, true, false]}
+        /** 旋转：只允许绕 Z */
+        disableRotations={false}
+        rotationAxes={[false, false, true]}
+        /** 缩放：只允许 XY */
+        disableScaling={false}
+        scaleAxes={[true, true, false]}
+        /** 拖拽开始 / 结束 */
+        onDragStart={() => {
+          mapControls.enabled = false;
+        }}
+        onDragEnd={() => {
+          mapControls.enabled = true;
+
+          updateArea(area.id, {
+            center: meshRef.current.position.clone(),
+            rotation: meshRef.current.rotation.z,
+            width: meshRef.current.scale.x,
+            height: meshRef.current.scale.y,
+          });
+        }}
+      >
+        <mesh
+          ref={meshRef}
+          position={area.center}
+          rotation={[0, 0, area.rotation ?? 0]}
+          scale={[area.width, area.height, 1]}
+          onPointerDown={(e) => {
+            e.stopPropagation();
+            select([area.id]);
+          }}
+        >
+          <planeGeometry args={[1, 1]} />
+          <meshBasicMaterial color={selected ? '#fab005' : '#51cf66'} transparent opacity={0.4} />
+        </mesh>
+      </PivotControls>
+      {/* <TransformControls
+        onMouseUp={() => {
+          const mesh = meshRef.current;
+          updateArea(area.id, {
+            width: mesh.scale.x,
+            height: mesh.scale.y,
+            center: mesh.position.clone(),
+          });
+        }}
+        object={meshRef}
+        mode='scale' // 'translate' | 'rotate' | 'scale'
+      /> */}
+    </group>
   );
 }
