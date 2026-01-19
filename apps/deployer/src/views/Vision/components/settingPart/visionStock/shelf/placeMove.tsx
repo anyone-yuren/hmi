dff6d8d80452578b2a1fa5d9e6a86678ff8d299b
wangxiaohua <>
2026-01-19 16:01:38 +0800

    feat(deployer): 视觉放货新增front

--- apps/deployer/src/views/Vision/components/settingPart/visionStock/shelf/placeMove.tsx
+++ apps/deployer/src/views/Vision/components/settingPart/visionStock/shelf/placeMove.tsx
@@ -29,6 +29,10 @@ const PlaceMove = (props: IProps) => {
     extra_height: 0,
     pallet_model_list: [],
     compensation: {
+      front: {
+        offset_x: [],
+        offset_y: [],
+      },
       left: {
         offset_x: [],
         offset_y: [],
@@ -62,6 +66,10 @@ const PlaceMove = (props: IProps) => {
     const translateHashMap: any = {
       compensation: (originState: any) => {
         const obj: any = {
+          front: {
+            offset_x: originState?.['compensation']?.['front']?.['offset_x']?.value,
+            offset_y: originState?.['compensation']?.['front']?.['offset_y']?.value,
+          },
           left: {
             offset_x: originState?.['compensation']?.['left']?.['offset_x']?.value,
             offset_y: originState?.['compensation']?.['left']?.['offset_y']?.value,
@@ -278,6 +286,17 @@ const PlaceMove = (props: IProps) => {
                 compensation: (originState: any, hashMap: any) => {
                   const obj = {
                     ...originState['compensation'],
+                    font: {
+                      ...originState['compensation']['front'],
+                      offset_x: {
+                        ...originState['compensation']['front']['offset_x'],
+                        value: hashMap['compensation']['front']['offset_x'],
+                      },
+                      offset_y: {
+                        ...originState['compensation']['front']['offset_y'],
+                        value: hashMap['compensation']['front']['offset_y'],
+                      },
+                    },
                     left: {
                       ...originState['compensation']['left'],
                       offset_x: {
