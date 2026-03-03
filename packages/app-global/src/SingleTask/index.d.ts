d3cfab994180f81ae5586bb32a16402bdadc931b
wangxiaohua <>
2026-03-03 15:11:33 +0800

    feat(deployer): 恢复单机任务

--- packages/app-global/src/SingleTask/index.d.ts
+++ packages/app-global/src/SingleTask/index.d.ts
@@ -1,5 +1,3 @@
-import Konva from 'konva';
-
 export interface IPoint {
   id: string;
   type: number;
@@ -20,7 +18,7 @@ export interface ITaskItem {
   task_group_id: string;
   loop_count?: number;
   task_interval?: string;
-  task_state: "Init" | "Running" | "Completed" | "Cancel" | "Error";
+  task_state: "Init" | "Running" | "Completed" | "Cancel" | "Error" | "Stop";
   tasks: ISubTaskItem[];
 }
 
@@ -28,9 +26,9 @@ export interface ISubTaskItem {
   task_id: string;
   task_point_id: string;
   task_type: "Pick" | "Place" | "Null" | "Charge";
-  task_state: "Init" | "Running" | "Completed" | "Cancel" | "Error";
-  loop_count: string
+  task_state: "Init" | "Running" | "Completed" | "Cancel" | "Error" | "Stop";
+  loop_count: string;
 }
 
 // 0调度 3单机 100其他
-export type IMode = 0 | 3 | 100
+export type IMode = 0 | 3 | 100;
--- packages/app-global/src/SingleTask/useConstants.ts
+++ packages/app-global/src/SingleTask/useConstants.ts
@@ -1,54 +1,61 @@
-import { useTranslation } from 'react-i18next';
-import { ISubTaskItem } from './index.d';
+import { useTranslation } from "react-i18next";
+import { ISubTaskItem } from "./index.d";
 const useConstants = () => {
   const { t } = useTranslation();
-  const TaskStatusHashMap: Record<ISubTaskItem['task_state'] | 0 | 1, { text: string; color: string }> = {
+  const TaskStatusHashMap: Record<
+    ISubTaskItem["task_state"] | 0 | 1,
+    { text: string; color: string }
+  > = {
     0: {
-      text: t('common.taskStatus.unexecuted'),
-      color: '#aeaeae',
+      text: t("common.taskStatus.unexecuted"),
+      color: "#aeaeae",
     },
     1: {
-      text: t('common.taskStatus.running'),
-      color: '#00D1D1',
+      text: t("common.taskStatus.running"),
+      color: "#00D1D1",
     },
     Init: {
-      text: t('common.taskStatus.unexecuted'),
-      color: '#aeaeae',
+      text: t("common.taskStatus.unexecuted"),
+      color: "#aeaeae",
     },
     Running: {
-      text: t('common.taskStatus.running'),
-      color: '#00D1D1',
+      text: t("common.taskStatus.running"),
+      color: "#00D1D1",
     },
     Completed: {
-      text: t('common.taskStatus.success'),
-      color: '#009688',
+      text: t("common.taskStatus.success"),
+      color: "#009688",
     },
     Error: {
-      text: t('common.taskStatus.error'),
-      color: '#f44336',
+      text: t("common.taskStatus.error"),
+      color: "#f44336",
     },
     Cancel: {
-      text: t('common.taskStatus.cancel'),
-      color: '#ff9800',
+      text: t("common.taskStatus.cancel"),
+      color: "#ff9800",
+    },
+    Stop: {
+      text: t("common.taskStatus.stop"),
+      color: "#FFEB3B",
     },
   };
 
-  const TaskTypeHashMap: Record<ISubTaskItem['task_type'], string> = {
-    Pick: t('common.taskState.pickUp'),
-    Null: t('common.taskState.moving'),
-    Charge: t('common.taskState.charging'),
-    Place: t('common.taskState.pickDown'),
+  const TaskTypeHashMap: Record<ISubTaskItem["task_type"], string> = {
+    Pick: t("common.taskState.pickUp"),
+    Null: t("common.taskState.moving"),
+    Charge: t("common.taskState.charging"),
+    Place: t("common.taskState.pickDown"),
   };
 
   const vertexTypeHashMap: any = {
-    0: t('deployer.singleTask.commonPoint'),
-    1: t('deployer.singleTask.warehousePoint'),
-    2: t('deployer.singleTask.homePoint'),
-    3: t('deployer.singleTask.turnRoundPoint'),
-    4: t('deployer.singleTask.stereoWarehousePoint'),
-    5: t('deployer.singleTask.visionPoint'),
-    6: t('deployer.singleTask.chargePoint'),
-    7: t('deployer.singleTask.devicePoint'),
+    0: t("deployer.singleTask.commonPoint"),
+    1: t("deployer.singleTask.warehousePoint"),
+    2: t("deployer.singleTask.homePoint"),
+    3: t("deployer.singleTask.turnRoundPoint"),
+    4: t("deployer.singleTask.stereoWarehousePoint"),
+    5: t("deployer.singleTask.visionPoint"),
+    6: t("deployer.singleTask.chargePoint"),
+    7: t("deployer.singleTask.devicePoint"),
   };
   return { TaskStatusHashMap, TaskTypeHashMap, vertexTypeHashMap };
 };
