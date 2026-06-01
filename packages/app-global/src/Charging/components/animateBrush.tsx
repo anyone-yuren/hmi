9c475758765681920ece90237de4c713396c5885
wangxiaohua <>
2026-06-01 17:28:39 +0800

    feat(deployer): 充电页面多语言显示的问题修复

--- packages/app-global/src/Charging/components/animateBrush.tsx
+++ packages/app-global/src/Charging/components/animateBrush.tsx
@@ -1,13 +1,13 @@
-import { useVehicleStore } from '@gbeata/store';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import { Button } from 'antd';
-import dayjs from 'dayjs';
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useShallow } from 'zustand/react/shallow';
-import Flash from './flash';
-import LoadingCharging from './loadingCharging';
+import { useVehicleStore } from "@gbeata/store";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Button } from "antd";
+import dayjs from "dayjs";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useShallow } from "zustand/react/shallow";
+import Flash from "./flash";
+import LoadingCharging from "./loadingCharging";
 const AnimateBrush = (props) => {
   const { setPowerStatus, powerStatus, chargePileStatus } = useVehicleStore(
     useShallow((state) => {
@@ -18,7 +18,7 @@ const AnimateBrush = (props) => {
       };
     }),
   );
-  const [threeColor, setThreeColor] = useState('green');
+  const [threeColor, setThreeColor] = useState("green");
   const [isBrush, setIsBrush] = useState(false);
   const [isStation, setIsStation] = useState(false);
   const [stretch, setStretch] = useState(false);
@@ -38,10 +38,10 @@ const AnimateBrush = (props) => {
     }[]
   >([]);
 
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const initViewState = () => {
-    setThreeColor('green');
+    setThreeColor("green");
     setPowerStatus({
       ...powerStatus,
       // power: 0,
@@ -75,51 +75,51 @@ const AnimateBrush = (props) => {
     if (chargePileStatus.pe_charge_output) {
       setIsBrush(true);
       setIsLoading(false);
-      const timeString = dayjs().format('HH:mm:ss');
+      const timeString = dayjs().format("HH:mm:ss");
       setVehicleChargingData([
         {
-          key: '1',
-          message: t('common.charging.vehicleSendLight'),
+          key: "1",
+          message: t("common.charging.vehicleSendLight"),
           time: timeString,
         },
         {
-          key: '2',
-          message: t('common.charging.waitChargeStationLight'),
+          key: "2",
+          message: t("common.charging.waitChargeStationLight"),
           time: timeString,
         },
         {
-          key: '3',
-          message: t('common.charging.waitChargeStationOn'),
+          key: "3",
+          message: t("common.charging.waitChargeStationOn"),
           time: timeString,
         },
       ]);
     }
-  }, [chargePileStatus.pe_charge_output]);
+  }, [chargePileStatus.pe_charge_output, i18n.language]);
   useEffect(() => {
     if (chargePileStatus.brush_board_status === 0) {
       setStretch(true);
-      const timeString = dayjs().format('HH:mm:ss');
+      const timeString = dayjs().format("HH:mm:ss");
       setStationChargingData([
         {
-          key: '1',
-          message: t('common.charging.chargeStationOn'),
+          key: "1",
+          message: t("common.charging.chargeStationOn"),
           time: timeString,
         },
       ]);
     }
-  }, [chargePileStatus.brush_board_status]);
+  }, [chargePileStatus.brush_board_status, i18n.language]);
 
   useEffect(() => {
     // 充电桩光电触发
     if (chargePileStatus.pe_charge_input) {
       setIsStation(true);
-      const timeString = dayjs().format('HH:mm:ss');
+      const timeString = dayjs().format("HH:mm:ss");
       setStationChargingData((origin) => {
         return [
           ...origin,
           {
-            key: '3',
-            message: t('common.charging.chargingStationLight'),
+            key: "3",
+            message: t("common.charging.chargingStationLight"),
             time: timeString,
           },
         ];
@@ -127,12 +127,12 @@ const AnimateBrush = (props) => {
     }
   }, [chargePileStatus.pe_charge_input]);
   return (
-    <div className='w-full flex flex-1  relative'>
+    <div className="w-full flex flex-1  relative">
       {false && (
-        <div className='absolute flex gap-2 p-4 z-50'>
+        <div className="absolute flex gap-2 p-4 z-50">
           <Button
-            type='primary'
-            size='small'
+            type="primary"
+            size="small"
             onClick={() => {
               setIsBrush(!isBrush);
               setIsLoading(false);
@@ -141,14 +141,14 @@ const AnimateBrush = (props) => {
                   ? []
                   : [
                       {
-                        key: '1',
-                        message: '车辆发送光电',
-                        time: '2023-01-01 12:00:00',
+                        key: "1",
+                        message: "车辆发送光电",
+                        time: "2023-01-01 12:00:00",
                       },
                       {
-                        key: '2',
-                        message: '等待充电桩伸出',
-                        time: '2023-01-01 12:00:00',
+                        key: "2",
+                        message: "等待充电桩伸出",
+                        time: "2023-01-01 12:00:00",
                       },
                     ],
               );
@@ -157,8 +157,8 @@ const AnimateBrush = (props) => {
             光电触发
           </Button>
           <Button
-            type='primary'
-            size='small'
+            type="primary"
+            size="small"
             onClick={() => {
               setIsStation(!isStation);
               setStretch(false);
@@ -167,14 +167,14 @@ const AnimateBrush = (props) => {
                   ? []
                   : [
                       {
-                        key: '1',
-                        message: '充电桩伸出',
-                        time: '2023-01-01 12:00:00',
+                        key: "1",
+                        message: "充电桩伸出",
+                        time: "2023-01-01 12:00:00",
                       },
                       {
-                        key: '2',
-                        message: '等待充电桩发光',
-                        time: '2023-01-01 12:00:00',
+                        key: "2",
+                        message: "等待充电桩发光",
+                        time: "2023-01-01 12:00:00",
                       },
                     ],
               );
@@ -182,17 +182,27 @@ const AnimateBrush = (props) => {
           >
             充电桩光电
           </Button>
-          <Button variant='solid' color='red' size='small' onClick={() => setStretch(!stretch)}>
+          <Button
+            variant="solid"
+            color="red"
+            size="small"
+            onClick={() => setStretch(!stretch)}
+          >
             伸缩机械臂
           </Button>
-          <Button variant='solid' color='yellow' size='small' onClick={() => setIsLoading(!isLoading)}>
+          <Button
+            variant="solid"
+            color="yellow"
+            size="small"
+            onClick={() => setIsLoading(!isLoading)}
+          >
             准备充电
           </Button>
           <Button
-            type='primary'
-            size='small'
+            type="primary"
+            size="small"
             onClick={() => {
-              setThreeColor('yellow');
+              setThreeColor("yellow");
               setPowerStatus({
                 power: 50,
                 charge_status: 3,
@@ -202,11 +212,11 @@ const AnimateBrush = (props) => {
             开始充电
           </Button>
           <Button
-            variant='solid'
-            color='red'
-            size='small'
+            variant="solid"
+            color="red"
+            size="small"
             onClick={() => {
-              setThreeColor('green');
+              setThreeColor("green");
               setPowerStatus({
                 ...powerStatus,
                 power: 0,
@@ -223,16 +233,16 @@ const AnimateBrush = (props) => {
             测试停止
           </Button>
           <Button
-            variant='solid'
-            color='yellow'
-            size='small'
+            variant="solid"
+            color="yellow"
+            size="small"
             onClick={() => {
               setVehicleChargingData([
                 ...vehicleChargingData,
                 {
-                  key: '3',
-                  message: '车辆发送光电',
-                  time: '2023-01-01 12:00:00',
+                  key: "3",
+                  message: "车辆发送光电",
+                  time: "2023-01-01 12:00:00",
                 },
               ]);
             }}
@@ -242,36 +252,36 @@ const AnimateBrush = (props) => {
         </div>
       )}
       {/* 小车 */}
-      <div className='flex-1 relative'>
+      <div className="flex-1 relative">
         <div
-          className='absolute  z-40 top-1/2 -translate-y-1/2 w-1/3 h-2/3 bg-gradient-to-l from-white/40 to-white/0'
-          style={{ right: '80px' }}
+          className="absolute  z-40 top-1/2 -translate-y-1/2 w-1/3 h-2/3 bg-gradient-to-l from-white/40 to-white/0"
+          style={{ right: "80px" }}
         >
           {/* 俯视截面 */}
-          <div className='w-12 h-[calc(100%+4px)] absolute -top-[2px] -right-12  bg-gradient-to-r from-white/40 to-white/10 [clip-path:polygon(-10%_0%,100%_6%,100%_94%,-10%_100%)]'></div>
+          <div className="w-12 h-[calc(100%+4px)] absolute -top-[2px] -right-12  bg-gradient-to-r from-white/40 to-white/10 [clip-path:polygon(-10%_0%,100%_6%,100%_94%,-10%_100%)]"></div>
           {/* 传感器 */}
-          <div className='w-1 h-20 absolute top-1/2 -right-10 flex flex-col justify-between '>
-            <div className='w-1 h-3 bg-gradient-to-r from-white/80 to-white/40 rounded-[1px]'>
+          <div className="w-1 h-20 absolute top-1/2 -right-10 flex flex-col justify-between ">
+            <div className="w-1 h-3 bg-gradient-to-r from-white/80 to-white/40 rounded-[1px]">
               <div
-                className='absolute top-0'
+                className="absolute top-0"
                 style={{
-                  width: '92px',
-                  right: '-92px',
+                  width: "92px",
+                  right: "-92px",
                 }}
               >
-                {isBrush && <Flash type='car' />}
+                {isBrush && <Flash type="car" />}
               </div>
             </div>
-            <div className='w-1 h-3 bg-gradient-to-r from-white/80 to-white/40 rounded-[1px]'></div>
+            <div className="w-1 h-3 bg-gradient-to-r from-white/80 to-white/40 rounded-[1px]"></div>
           </div>
-          <div className='w-1 h-20 bg-white/80 absolute top-1/2 -right-12'></div>
-          <div className='w-1 h-20 absolute top-1/2 -right-12 translate-x-1 bg-white/30 [clip-path:polygon(-10%_0%,100%_3%,100%_97%,-10%_100%)]'></div>
+          <div className="w-1 h-20 bg-white/80 absolute top-1/2 -right-12"></div>
+          <div className="w-1 h-20 absolute top-1/2 -right-12 translate-x-1 bg-white/30 [clip-path:polygon(-10%_0%,100%_3%,100%_97%,-10%_100%)]"></div>
 
           {/* 充电信息 */}
-          <div className='absolute -left-full flex flex-col divide-y divide-slate-400/40'>
+          <div className="absolute -left-full flex flex-col divide-y divide-slate-400/40">
             {vehicleChargingData.map((item, index) => (
               <motion.div
-                className='py-2 flex items-center justify-center gap-[2px]'
+                className="py-2 flex items-center justify-center gap-[2px]"
                 key={item.key}
                 initial={{ opacity: 0, x: -20 }}
                 animate={{ opacity: 1, x: 0 }}
@@ -280,10 +290,10 @@ const AnimateBrush = (props) => {
                   delay: 0.5 * index,
                 }}
               >
-                <CheckCircleIcon style={{ color: '#4caf50' }} size='large' />
-                <div className='flex-1'>
-                  <div className='text-white font-bold'>{item.message}</div>
-                  <div className='text-xs text-white/50'>{item.time}</div>
+                <CheckCircleIcon style={{ color: "#4caf50" }} size="large" />
+                <div className="flex-1">
+                  <div className="text-white font-bold">{item.message}</div>
+                  <div className="text-xs text-white/50">{item.time}</div>
                 </div>
               </motion.div>
             ))}
@@ -291,31 +301,31 @@ const AnimateBrush = (props) => {
         </div>
       </div>
       {/* 刷版 */}
-      <div className='flex-1 relative'>
+      <div className="flex-1 relative">
         <div
-          className='absolute top-1/2 -translate-y-1/2 w-1/3 h-2/3 bg-gradient-to-r from-teal-400/60 to-white/0'
-          style={{ left: '80px' }}
+          className="absolute top-1/2 -translate-y-1/2 w-1/3 h-2/3 bg-gradient-to-r from-teal-400/60 to-white/0"
+          style={{ left: "80px" }}
         >
           {/* 开关 */}
-          <div className='absolute top-8 left-1/2 translate-x-1/2 w-6 h-6 bg-gradient-to-r from-yellow-500 to-yellow-700/40 rounded-full flex flex-col justify-between items-center '>
-            <div className='flex-1 flex items-center justify-between relative'>
-              <div className='w-3 h-3 bg-gradient-to-r from-red-500 to-red-800 rounded-full absolute left-1/2 -translate-x-1/2 shadow-[1px_0_10px_1px_rgba(0,0,0,0.8)]'></div>
+          <div className="absolute top-8 left-1/2 translate-x-1/2 w-6 h-6 bg-gradient-to-r from-yellow-500 to-yellow-700/40 rounded-full flex flex-col justify-between items-center ">
+            <div className="flex-1 flex items-center justify-between relative">
+              <div className="w-3 h-3 bg-gradient-to-r from-red-500 to-red-800 rounded-full absolute left-1/2 -translate-x-1/2 shadow-[1px_0_10px_1px_rgba(0,0,0,0.8)]"></div>
             </div>
           </div>
           {/* 俯视截面 */}
-          <div className='w-12 h-full absolute top-0 -left-12  bg-gradient-to-l  from-teal-400/60 to-teal-600/10 [clip-path:polygon(-10%_6%,100%_0%,100%_100%,-10%_94%)]'></div>
+          <div className="w-12 h-full absolute top-0 -left-12  bg-gradient-to-l  from-teal-400/60 to-teal-600/10 [clip-path:polygon(-10%_6%,100%_0%,100%_100%,-10%_94%)]"></div>
           {/* 传感器 */}
-          <div className='w-1 h-20 absolute top-1/2 -left-7 flex flex-col justify-between'>
-            <div className='w-1 h-3 bg-gradient-to-l from-white/80 to-white/40 rounded-[1px]'></div>
-            <div className='w-1 h-3 bg-gradient-to-l from-white/80 to-white/40 rounded-[1px] relative'>
+          <div className="w-1 h-20 absolute top-1/2 -left-7 flex flex-col justify-between">
+            <div className="w-1 h-3 bg-gradient-to-l from-white/80 to-white/40 rounded-[1px]"></div>
+            <div className="w-1 h-3 bg-gradient-to-l from-white/80 to-white/40 rounded-[1px] relative">
               <div
-                className='absolute top-0 '
+                className="absolute top-0 "
                 style={{
-                  width: '92px',
-                  left: '-92px',
+                  width: "92px",
+                  left: "-92px",
                 }}
               >
-                {isStation && <Flash type='brush' />}
+                {isStation && <Flash type="brush" />}
               </div>
             </div>
           </div>
@@ -324,28 +334,28 @@ const AnimateBrush = (props) => {
             initial={{ x: -16 }}
             animate={stretch ? { x: -84 } : { x: -16 }}
             transition={{ duration: 1.5 }}
-            className='w-2 h-20 absolute top-1/2 -left-8'
+            className="w-2 h-20 absolute top-1/2 -left-8"
           >
-            <div className='w-4 h-24 absolute -top-2 bg-gradient-to-l from-white/80 to-white/50'></div>
-            <div className='w-2 h-full bg-black/30 absolute'></div>
+            <div className="w-4 h-24 absolute -top-2 bg-gradient-to-l from-white/80 to-white/50"></div>
+            <div className="w-2 h-full bg-black/30 absolute"></div>
             <motion.div
               initial={{ width: 0 }}
               animate={stretch ? { width: 68 } : { width: 0 }}
               transition={{ duration: 1.5 }}
-              className='h-2 bg-white/60 absolute top-2 left-4'
+              className="h-2 bg-white/60 absolute top-2 left-4"
             ></motion.div>
             <motion.div
               initial={{ width: 0 }}
               animate={stretch ? { width: 68 } : { width: 0 }}
               transition={{ duration: 1.5 }}
-              className='h-2 bg-white/60 absolute bottom-2 left-4'
+              className="h-2 bg-white/60 absolute bottom-2 left-4"
             ></motion.div>
           </motion.div>
           {/* 充电信息 */}
-          <div className='absolute -right-full flex flex-col divide-y divide-slate-400/40'>
+          <div className="absolute -right-full flex flex-col divide-y divide-slate-400/40">
             {stationChargingData.map((item, index) => (
               <motion.div
-                className='py-2 flex items-center justify-center gap-[2px]'
+                className="py-2 flex items-center justify-center gap-[2px]"
                 key={item.key}
                 initial={{ opacity: 0, x: 20 }}
                 animate={{ opacity: 1, x: 0 }}
@@ -354,17 +364,17 @@ const AnimateBrush = (props) => {
                   delay: 0.5 * index,
                 }}
               >
-                <CheckCircleIcon style={{ color: '#4caf50' }} size='large' />
-                <div className='flex-1'>
-                  <div className='text-white font-bold'>{item.message}</div>
-                  <div className='text-xs text-white/50'>{item.time}</div>
+                <CheckCircleIcon style={{ color: "#4caf50" }} size="large" />
+                <div className="flex-1">
+                  <div className="text-white font-bold">{item.message}</div>
+                  <div className="text-xs text-white/50">{item.time}</div>
                 </div>
               </motion.div>
             ))}
           </div>
           {/* 充电桩上的面板 */}
-          <div className='absolute top-1/2 left-1/2 -translate-y-2 -translate-x-3 w-20 h-24 bg-gradient-to-r from-black/70 to-white/0 rounded-sm p-4'>
-            <div className='w-full h-full bg-gradient-to-r from-white/70 to-white/0 rounded-sm'></div>
+          <div className="absolute top-1/2 left-1/2 -translate-y-2 -translate-x-3 w-20 h-24 bg-gradient-to-r from-black/70 to-white/0 rounded-sm p-4">
+            <div className="w-full h-full bg-gradient-to-r from-white/70 to-white/0 rounded-sm"></div>
           </div>
         </div>
       </div>
